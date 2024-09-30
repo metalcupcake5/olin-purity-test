cbdf7bd295435c3e15aa2a25f6769e9652981e41
metalcupcake5 <imcoolcom5@gmail.com>
2024-09-29 23:39:13 -0400

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import questions from '$lib/data.json';
+	let done = [];
+</script>
+
+<form>
+	{#each questions as question}
+		<label>
+			<input type="checkbox" name="flavours" value={question} bind:group={done} />
+
+			{question}
+			<br />
+		</label>
+	{/each}
+</form>
+
+{done.length}
