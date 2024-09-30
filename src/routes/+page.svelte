25ed2a7f1d40d958cfb0dc50cddc5a8359a4d198
metalcupcake5 <imcoolcom5@gmail.com>
2024-09-30 00:08:05 -0400

    source of questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,10 @@
 <h1>your score: {done.length}</h1>
 
 <footer>
-	source:
+	questions taken from <a href="https://franklyspeakingnews.com/2022/03/the-olin-purity-test/"
+		>https://franklyspeakingnews.com/2022/03/the-olin-purity-test/</a
+	><br />
+	site source:
 	<a href="https://github.com/metalcupcake5/olin-purity-test"
 		><i class="fa-brands fa-github"></i> metalcupcake5/olin-purity-test</a
 	>
