0d2592ec178040d45131c0168da1caff03ae396e
metalcupcake5 <imcoolcom5@gmail.com>
2024-10-04 16:17:18 -0400

    add disclaimer, update other stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,11 @@
 
 {#if form?.success}
 	<div style="outline: solid 2px;">
-		<h1>your score is {form.score}</h1>
+		<h1>your score is {form.score}/{questions.length}</h1>
 		<span>
-			your score has been logged<br />please do not submit multiple times it will mess up data
-			collection 3:
+			your score has been logged
+			<br />
+			please do not submit multiple times it will mess up data collection 3:
 		</span>
 	</div>
 {/if}
@@ -21,11 +22,16 @@
 	<meta property="og:description" content="yeah" />
 </svelte:head>
 
-<div style="display: flex;flex-direction: column;gap: 2em;">
+<div style="display: flex;flex-direction: column;gap: 1em;">
 	<!-- yeah im too lazy to use style tags what about it -->
 	<div>
 		<h1>olin purity test</h1>
-		<span>score is automatically calculated at the bottom</span>
+		<strong>
+			Caution: This is not intended to be a bucket list. Completion of all items on this test will
+			likely result in becoming Olin Man.
+		</strong>
+		<br />
+		<span>submit score at the bottom</span>
 	</div>
 	<form method="POST">
 		{#each questions as question, i}
@@ -37,15 +43,16 @@
 		{/each}
 		<br />
 
-		<!-- <input type="text" name="name" placeholder="Name(optional)" /> -->
 		<span>
-			submitting this form will log your score, please do not submit multiple times it will mess up
-			data collection 3:
+			submitting this form will log your score, please make sure to complete accurately and to not
+			submit multiple times it will mess up data collection 3:
 		</span>
 		<br />
 		<sub style="font-size:xx-small">
-			in all honesty it really does not matter (i am not doing anything with the data anyways) but
-			it would be cool to look at you guys should get datapilled imagine all the graphs i could make
+			in all honesty it really does not matter (we are not doing anything with the data anyways) but
+			it would be cool to look at
+			<br />
+			you guys should get datapilled imagine all the graphs we could make
 		</sub>
 		<br />
 		<input type="text" name="name" placeholder="Name (optional)" />
